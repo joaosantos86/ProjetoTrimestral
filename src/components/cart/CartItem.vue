831ce3f287dad799f4705312921c0134f70ac898
OtavioAugustodaRoza <otavioaugustodaroza>
2026-06-19 11:15:07 -0300

    FEAT: comecei a fazer a task mas ta errado por enquanto

---
+++ src/components/cart/CartItem.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+
+const props = defineProps({
+  item:Object,
+})
+</script>
+
+<template>
+
+  <div>
+  <img :src="props.item.image" :alt="props.item.name">
+  <div>
+  <div>
+  <p>{{ props.item.name }}</p>
+  <span>{{ props.item.description }}</span>
+  </div>
+  <div>
+  <span>{{ props.item.price }}</span>
+  <span>quantidade: {{props.item.quantity}}</span>
+  </div>
+  </div>
+  </div>
+
+
+</template>
+
+<style scoped>
+
+</style>
