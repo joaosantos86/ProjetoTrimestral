514e882a1e400abe5bd1bd9b0ed62686412dcf85
OtavioAugustodaRoza <otavioaugustodaroza@gmail.com>
2026-06-20 10:32:56 -0300

    fix: init

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import CartView from '@/views/CartView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'cart',
-      component: CartView,
-    },
-  ],
+  routes: [],
 })
 
 export default router
