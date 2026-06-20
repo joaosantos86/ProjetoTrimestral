34ad63534af84ea25045414213ebeea023926ae6
joaosantos86 <joaops41224@gmail.com>
2026-06-19 23:45:39 -0300

    FIX: Removendo mensagem vue

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,6 @@ import AppHeader from './components/layout/AppHeader.vue';
     <AppHeader />
   </header>
   <RouterView />
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
 </template>
 
 <style scoped></style>
